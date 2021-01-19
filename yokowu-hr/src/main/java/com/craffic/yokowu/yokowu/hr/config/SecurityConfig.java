d3925e6f1ac931d178990e0c5f53a3d8a53574a2
Craffic <Craffic82997916>
2021-01-19 22:20:29 +0800

    1. 添加前端页面，实现登录成功跳转，失败回调，注销设置

--- yokowu-hr/src/main/java/com/craffic/yokowu/yokowu/hr/config/SecurityConfig.java
+++ yokowu-hr/src/main/java/com/craffic/yokowu/yokowu/hr/config/SecurityConfig.java
@@ -3,6 +3,8 @@ package com.craffic.yokowu.yokowu.hr.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -15,6 +17,43 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     PasswordEncoder passwordEncoder(){
         // 暂时使用明文存储，方法已过期必须使用加密密码
         return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/js/**", "/images/**", "/css/**");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().authenticated().
+                and().
+                formLogin().
+                loginPage("/login.html").
+                loginProcessingUrl("/doLogin").
+                usernameParameter("userName").
+                passwordParameter("password").
+//--------------登录成功的分两种情况-----------------------------------------------
+                // 服务端跳转：不管从哪个页面来的，登录成功后地址都是/success
+//                successForwardUrl("/success").
+                // 重定向: 会记录登录前的地址，比如登录前的地址是/hello，登录后就跳转到hello页面上
+                // defaultSuccessUrl("/success", true) =  successForwardUrl("/success")
+                defaultSuccessUrl("/success").
+//--------------登录失败的都跳转到一个失败页面-----------------------------------------------
+                // failureForwardUrl: 服务端跳转；failureUrl：重定向
+                failureForwardUrl("/failure").
+//                failureUrl("/failure").
+//--------------注销logout默认是get请求，也可以post请求----------------------------------------------
+                and().
+                logout().
+                logoutSuccessUrl("/login.html").
+                // 清除session和authentication认证信息，不用配，默认就会清除
+                invalidateHttpSession(true).
+                clearAuthentication(true).
+                permitAll().
+                and().
+                csrf().disable();
     }
 
     // 配置用户名密码（配置在内存里）
--- yokowu-hr/src/main/java/com/craffic/yokowu/yokowu/hr/controller/LoginController.java
+++ yokowu-hr/src/main/java/com/craffic/yokowu/yokowu/hr/controller/LoginController.java
@@ -1,6 +1,7 @@
 package com.craffic.yokowu.yokowu.hr.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,4 +10,15 @@ public class LoginController {
     public String hello(){
         return "hello Oauth2";
     }
+
+    @RequestMapping("/success")
+    public String success(){
+        return "login success";
+    }
+
+    @RequestMapping("/failure")
+    public String failure(){
+        return "login failure";
+    }
+
 }
